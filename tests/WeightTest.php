1f4ee5259de0d34b5f7614c32e652630642a8683
nurs <programmer.kg@mail.ru>
2021-03-19 05:53:31 +0300

    php fixer

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -8,8 +8,8 @@ use PHPUnit\Framework\TestCase;
 class WeightTest extends TestCase
 {
     /** @test */
-    public function it_can_convert_kilograms_to_lbs() {
-
+    public function it_can_convert_kilograms_to_lbs()
+    {
         $lbs = Weight::fromKilograms(100)->toLbs();
 
         $this->assertEquals(220.4623, $lbs);
