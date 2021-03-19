cba697b4ae40722855a53ab0eb8d04c4e7a21225
nurs <programmer.kg@mail.ru>
2021-03-19 06:55:43 +0300

    add temperature conversion

---
+++ tests/TemperatureTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Indexcoder\UnitConversions\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Indexcoder\UnitConversions\Temperature;
+
+class TemperatureTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_celsius_to_fahrenheit()
+    {
+        $fahrenheit = Temperature::forCelsius(100)->toFahrenheit();
+
+        $this->assertEquals(212 , $fahrenheit);
+    }
+
+}
